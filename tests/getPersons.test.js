9a555d55687603227faf92d647cca179873e4da7
Shterion Yanev <shterion.yanev@yahoo.com>
2018-06-08 20:45:59 +0200

    Refactoring: using async/await in triggers

---
+++ tests/getPersons.test.js
@@ -0,0 +1,9 @@
+const config = require('./../config/config');
+const cfg = config.getEnvironment();
+const getPersons = require('./../lib/triggers/getPersons');
+
+describe('Test triggers', () => {
+  test('should get persons', () => {
+
+  });
+});
