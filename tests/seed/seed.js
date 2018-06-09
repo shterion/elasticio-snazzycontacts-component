db14822803866048523d6360c50677daf6ef6730
Shterion Yanev <shterion.yanev@yahoo.com>
2018-06-09 17:53:48 +0200

    Add more data in seed

--- tests/seed/seed.js
+++ tests/seed/seed.js
@@ -17,7 +17,34 @@ const configOptions = {
   path: cfg.path
 };
 
+const organizations = [{
+    name: 'Company Ltd.'
+  },
+  {
+    name: 'Test GmbH'
+  },
+  {
+    name: 'Travel Mates'
+  }
+];
+
+const persons = [{
+    name: 'Test 1',
+    firstname: 'Test 1'
+  },
+  {
+    name: 'Doe',
+    firstname: 'John'
+  },
+  {
+    name: 'Smith',
+    firstname: 'Mark'
+  },
+];
+
 module.exports = {
   options,
-  configOptions
+  configOptions,
+  persons,
+  organizations
 }
--- tests/triggers.test.js
+++ tests/triggers.test.js
@@ -4,6 +4,7 @@ const seed =  require('./seed/seed');
 const snazzy = require('./../lib/utils/snazzy');
 const getPersons = require('./../lib/triggers/getPersons');
 const getOrganizations = require('./../lib/triggers/getOrganizations');
+const { organizations, persons } = require('./seed/seed');
 
 describe('Test triggers', () => {
   test('should get cookie from the session', async () => {
@@ -29,4 +30,18 @@ describe('Test triggers', () => {
     const organizations = await getOrganizations.getOrganizations(seed.options);
     expect(organizations).toHaveLength(6);
   });
+
+  test('should get person name', async () => {
+    const cookie = await getOrganizations.session(seed.configOptions);
+    seed.options.uri =`${cfg.path}/mp_contact/json_respond/address_company/json_mainview?&mp_cookie=${cookie}`;
+    const person = await getPersons.getPersons(seed.options);
+    expect(person[0].name).toEqual(person[0].name);
+   });
+
+  test('should get organization name', async () => {
+    const cookie = await getOrganizations.session(seed.configOptions);
+    seed.options.uri =`${cfg.path}/mp_contact/json_respond/address_company/json_mainview?&mp_cookie=${cookie}`;
+    const organization = await getOrganizations.getOrganizations(seed.options);
+    expect(organization[0].name).toEqual(organizations[0].name);
+   });
 });
