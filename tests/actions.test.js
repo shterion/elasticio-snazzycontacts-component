6724ab5b038260dc9ca53f38463ab18672b1b11e
Shterion Yanev <shterion.yanev@yahoo.com>
2018-06-19 23:02:32 +0200

    Add tests for some actions

---
+++ tests/actions.test.js
@@ -0,0 +1,24 @@
+const config = require('./../config/config');
+const cfg = config.getEnvironment();
+const seed =  require('./seed/seed');
+const { createSession } = require('./../lib/utils/snazzy');
+const { organizations, persons, configOptions, options } = require('./seed/seed');
+const { checkForExistingUser, getSameContactId, createOrUpdatePerson } = require('./../lib/actions/createPerson');
+const toBeType = require('jest-tobetype');
+expect.extend(toBeType);
+
+describe('Test actions', () => {
+  test('should check for existning person', async () => {
+    const cookie = await createSession(configOptions);
+    const person = seed.persons[2];
+    const existningUserRowid = await checkForExistingUser(person, cookie);
+    expect(existningUserRowid).toHaveLength(6);
+  });
+
+  test('should get sameContactId', async () => {
+    const cookie = await createSession(configOptions);
+    const sameContactId = await getSameContactId(cookie);
+    expect(sameContactId).toBeType('number');
+    expect(sameContactId.toString()).toHaveLength(4);
+  });
+});
