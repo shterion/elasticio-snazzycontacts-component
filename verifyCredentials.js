4e9d88b13b781300444a0e871870dc0bf7065d36
Shterion Yanev <shterion.yanev@yahoo.com>
2018-06-20 12:17:13 +0200

    Modify credentials verification

--- verifyCredentials.js
+++ verifyCredentials.js
@@ -1,20 +1,46 @@
 "use strict";
+const config = require('./config/config');
+const cfg = config.getEnvironment();
+const { getToken, getCookie, createSession } = require('./lib/utils/snazzy');
 
-module.exports = function verifyCredentials(credentials, cb) {
+async function verifyCredentials(credentials, cb) {
   console.log('Credentials passed for verification %j', credentials)
 
-  if (!credentials.email) {
-    console.log('Invalid email');
-    return cb(null, {verified: false});
+  try {
+    const cfg = {
+      apikey: credentials.apikey,
+      email: credentials.email,
+      password: credentials.password,
+      path: credentials.path
+    };
+    const token = await getToken(cfg);
+    const cookie = await getCookie(token, cfg);
+    const session = await createSession(cfg);
+
+    cb(null, { verified: true });
+  } catch (e) {
+    console.log(`ERROR: ${e}`);
+    throw new Error(e);
   }
 
-  if (!credentials.apikey) {
-    console.log('Invalid API key');
-    return cb(null, {verified: false});
-  }
-
-  console.log('Credentials verified successfully');
-
-  cb(null, {verified: true});
+  // if (!credentials.email) {
+  //   console.log('Invalid email');
+  //   return cb(null, {
+  //     verified: false
+  //   });
+  // }
+  //
+  // if (!credentials.apikey) {
+  //   console.log('Invalid API key');
+  //   return cb(null, { verified: false });
+  // }
+  //
+  // console.log('Credentials verified successfully');
+  //
+  // cb(null, { verified: true });
 }
+verifyCredentials(cfg, () => {
+  console.log('Credentials verified successfully');
+});
 
+module.exports = {verifyCredentials};
